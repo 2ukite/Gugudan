173b9c4b4c4f2587b4a9caa7b5f6fb48aeacc568
2ukite <53216523+2ukite@users.noreply.github.com>
2022-04-12 19:34:57 +0900

    2단부터 9단까지 내 방식대로 만들어봄

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,12 +1,16 @@
  
 public class Gugudan {
 	public static void main(String[] args) {
-		// 2단
+		// 구구단 2단 부터 9단까지
+		int d = 2;
+		while (d<10) {
+			System.out.println(d + " 단");
 		int i = 1;
 		while (i<10) {
-			System.out.println("2단");
-			System.out.println("2 곱하기 "+i+" 은 "+2*i);
+			System.out.println(d + " 곱하기 "+ i +" 은 " + 2 * i);
 			i=i+1;
 		}
+		d=d+1;
+	}
 	}
 }
