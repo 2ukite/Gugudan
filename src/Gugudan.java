26c28a0af5a381f28ca603d4cbb19486dd858883
2ukite <53216523+2ukite@users.noreply.github.com>
2022-04-12 19:15:27 +0900

    구구단 2단

---
+++ src/Gugudan.java
@@ -0,0 +1,12 @@
+ 
+public class Gugudan {
+	public static void main(String[] args) {
+		// 2단
+		int i = 1;
+		while (i<10) {
+			System.out.println("2단");
+			System.out.println("2 곱하기 "+i+" 은 "+2*i);
+			i=i+1;
+		}
+	}
+}
