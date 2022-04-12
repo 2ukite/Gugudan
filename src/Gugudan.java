1871b92337930afbfde7c93d61251d83e90dd01f
2ukite <53216523+2ukite@users.noreply.github.com>
2022-04-13 00:37:58 +0900

    Scanner 클래스, System.out System.In 의 기초 의미 이해로 숫자 입력시 구구단 만들기

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,16 +1,23 @@
- 
+import java.util.Scanner;
+
 public class Gugudan {
 	public static void main(String[] args) {
-		// 구구단 2단 부터 9단까지
-		int d = 2;
-		while (d<10) {
-			System.out.println(d + " 단");
-		int i = 1;
-		while (i<10) {
-			System.out.println(d + " 곱하기 "+ i +" 은 " + 2 * i);
-			i=i+1;
-		}
-		d=d+1;
-	}
-	}
+		// 숫자를 입력해서 해당 단의 구구단 실행시키기
+		Scanner dan = new Scanner(System.in);
+		
+		int dan_number = dan.nextInt();
+			
+		System.out.println(dan_number + " 단");
+		
+		System.out.println(dan_number * 1);
+		System.out.println(dan_number * 2);
+		System.out.println(dan_number * 3);
+		System.out.println(dan_number * 4);
+		System.out.println(dan_number * 5);
+		System.out.println(dan_number * 6);
+		System.out.println(dan_number * 7);
+		System.out.println(dan_number * 8);
+		System.out.println(dan_number * 9);
+		
+}
 }
