afc342b270d9347c1bb68d14e2d6283dc2381404
2ukite <53216523+2ukite@users.noreply.github.com>
2022-04-14 02:04:27 +0900

    이중 for 문으로 구구단 만들기

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,23 +1,16 @@
-import java.util.Scanner;
-
 public class Gugudan {
 	public static void main(String[] args) {
-		// 숫자를 입력해서 해당 단의 구구단 실행시키기
-		// 2이상 9이하의 정수가 아닌 숫자 입력시 메세지 출력시키기
-		Scanner dan = new Scanner(System.in);
-
-		int dan_number = dan.nextInt();
+		// 이중 for 문으로 구구단 만들기 
 		
-		if (dan_number>=2 && dan_number <= 9) {
-
-		System.out.println("구구단 " + dan_number + " 단");
+		
+		for (int j=2; j<10; ++j) {
+			int dan_number = j;
+							
+		System.out.println(dan_number+" 단");
 
 		for (int i = 1; i<10; ++i) {
-		System.out.println(dan_number + " 곱하기 " + i + " 는 " + dan_number * i);
-			}
-		}
-		else {
-		System.out.println("2이상, 9 이하의 값만 입력할 수 있습니다.");	
+			System.out.println(dan_number+" 곱하기 "+ i + " 는 " + dan_number*i);
 			}
 		}
 	}
+}
