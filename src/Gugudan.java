c2bb75c1fe77f800d3f2d9eb53e2eff18174db81
2ukite <53216523+2ukite@users.noreply.github.com>
2022-04-14 02:46:14 +0900

    배열로 구구단 입력 / 이중 for 문으로 2~9단까지

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,16 +1,11 @@
 public class Gugudan {
 	public static void main(String[] args) {
-		// 이중 for 문으로 구구단 만들기 
-		
-		
-		for (int j=2; j<10; ++j) {
-			int dan_number = j;
-							
-		System.out.println(dan_number+" 단");
-
-		for (int i = 1; i<10; ++i) {
-			System.out.println(dan_number+" 곱하기 "+ i + " 는 " + dan_number*i);
+		int [] result = new int[9];
+		for (int j=2; j>=2&&j<=9; ++j) {
+			for (int i=0; i<result.length; i++) { // .length 배열의 길이 
+				result[i] = j*(i + 1);                // 배열에 값을 넣는다
+				System.out.println(result[i]);
 			}
-		}
+		}		
 	}
 }
